766c539209b989baaa156ec04e3a61781a36db86
Henrique Reinaldi <henriqreinaldi@gmail.com>
2025-09-02 21:29:31 -0300

    Add files via upload

--- command/command.py
+++ command/command.py
@@ -2,20 +2,20 @@ import sys
 from par import run
 
 print("command","="*80, "\n\n")
-if len(sys.argv) < 2:
-    print("Por favor, escolha algum modo! : run, ajuda")
+if len(sys.argv) < 1:
+    print("Por favor, declare o script a ser executado! : command script.command")
     sys.exit(1)
 
-elif sys.argv[1] == "run":
-    if len(sys.argv) < 3:
-        print("Por favor, selecione algum script! : command run script.command")
-        sys.exit(1)
-    nome = sys.argv[2]
-    modo = sys.argv[3] if len(sys.argv) > 3 else 0
+nome = sys.argv[1]
+modo = sys.argv[2] if len(sys.argv) > 2 else 0
+if ".command" != nome[-8:]:
+    print("tipo de arquivo errado! O script deve ser .command!")
+    sys.exit(1)
+
+try:
     with open(nome, 'r') as f:
         codigo = f.read()
-    run(codigo, modo)
-
-elif sys.argv[1] == "ajuda":
-    print("Confira a página do github para ver a documentação!")
+except:
+    print("Esse arquivo não existe!")
     sys.exit(1)
+run(codigo, modo)
---
+++ command/uso/exemplos/test.command
@@ -0,0 +1,8 @@
+load std
+
+set n to [1, 33+36, 3-3]
+set val to (-1) @n @n @n
+execute sum n
+apply to n
+show soma: n
+show val
